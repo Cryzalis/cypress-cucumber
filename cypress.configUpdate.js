5f364fbc3912ad4627db434e478a9373a29d6684
Cryzalis <dmytromaslo@gmail.com>
2022-10-09 19:34:06 +0300

    now all test pass

--- cypress.configUpdate.js
+++ cypress.configUpdate.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
 
       return config;
     },
-    defaultCommandTimeout:10000,
+    defaultCommandTimeout:12000,
     specPattern: "cypress/e2e/features/*.feature",
     baseUrl:"https://portal.telnyx.com",
     experimentalSessionAndOrigin: true,
